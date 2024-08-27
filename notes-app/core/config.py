81aaf559449d6411b64d6fae1da43da7c4607256
Aleksandr Rol <rolaleksandr75@gmail.com>
2024-08-27 23:10:05 +0900

    create db_manager and change db config

--- notes-app/core/config.py
+++ notes-app/core/config.py
@@ -1,17 +1,26 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, PostgresDsn
 from pydantic_settings import BaseSettings
+
 
 
 class SocketConfig(BaseModel):
     host: str = '0.0.0.0'
     port: int = 8000
     reload: bool
+    pool_size: int = 30
+    max_overflow: int = 10
+
+
+class DBConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
 
 
 class Settings(BaseSettings):
     app_name: str
     socket: SocketConfig = SocketConfig()
-    
+    db: DBConfig
 
 
 settings = Settings()
