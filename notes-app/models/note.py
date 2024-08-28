6b3d6eb599d6a056d9f9808b9141359012de38db
Aleksandr Rol <rolaleksandr75@gmail.com>
2024-08-29 07:26:41 +0900

    add fields name, text, user_id in note models and migration

--- notes-app/models/note.py
+++ notes-app/models/note.py
@@ -1,7 +1,10 @@
+from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 from core.base import Base
 
 
 class Note(Base):
-    pass
+    name: Mapped[str] = mapped_column(String(128), nullable=False)
+    text: Mapped[str]
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
