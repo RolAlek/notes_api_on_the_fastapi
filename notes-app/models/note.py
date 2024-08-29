c2f413e88b6039e9b8053eb2311ce8eb0a4caadc
Aleksandr Rol <rolaleksandr75@gmail.com>
2024-08-29 23:08:14 +0900

    add one to many relation for between user and note models

--- notes-app/models/note.py
+++ notes-app/models/note.py
@@ -1,10 +1,15 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from core.base import Base
 
+if TYPE_CHECKING:
+    from .user import User
 
 class Note(Base):
     name: Mapped[str] = mapped_column(String(128), nullable=False)
     text: Mapped[str]
     user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    user: Mapped['User'] = relationship(back_populates='notes')
--- notes-app/models/user.py
+++ notes-app/models/user.py
@@ -1,7 +1,12 @@
+from typing import TYPE_CHECKING
+
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from sqlalchemy.orm import Mapped, relationship
 
 from core.base import Base
 
+if TYPE_CHECKING:
+    from .note import Note
 
 class User(SQLAlchemyBaseUserTable[int], Base):
-    pass
+    notes: Mapped[list['Note']] = relationship(back_populates='user')
