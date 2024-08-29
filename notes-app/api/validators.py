23ca94c9ffab674991351cf2f27f9d1cc4fdb489
Aleksandr Rol <rolaleksandr75@gmail.com>
2024-08-29 23:39:14 +0900

    add note's name unique validator

---
+++ notes-app/api/validators.py
@@ -0,0 +1,24 @@
+from http import HTTPStatus
+
+from fastapi import HTTPException
+from models import Note, User
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+async def check_unique_note_name_for_user(
+    note_name: str,
+    user: User,
+    session: AsyncSession,
+) -> None:
+    user_note = await session.scalar(
+        select(Note).where(
+            Note.user_id == user.id,
+            Note.name == note_name,
+        )
+    )
+    if user_note is not None:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail=f"Заметка с названием {user_note.name} уже создана Вами.",
+        )
