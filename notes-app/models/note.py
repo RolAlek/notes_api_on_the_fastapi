4b85e8a414e4e1f87c37c31cadd9388637b9eb8c
Aleksandr Rol <rolaleksandr75@gmail.com>
2024-08-29 23:36:27 +0900

    add create_date field in Note model

--- notes-app/models/note.py
+++ notes-app/models/note.py
@@ -1,15 +1,18 @@
+from datetime import datetime
 from typing import TYPE_CHECKING
 
 from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from core.base import Base
+from core.db import Base
 
 if TYPE_CHECKING:
     from .user import User
+
 
 class Note(Base):
     name: Mapped[str] = mapped_column(String(128), nullable=False)
     text: Mapped[str]
     user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    create_date: Mapped[datetime] = mapped_column(default=datetime.now)
     user: Mapped['User'] = relationship(back_populates='notes')
--- notes-app/models/user.py
+++ notes-app/models/user.py
@@ -3,7 +3,7 @@ from typing import TYPE_CHECKING
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
 from sqlalchemy.orm import Mapped, relationship
 
-from core.base import Base
+from core.db import Base
 
 if TYPE_CHECKING:
     from .note import Note
